2adace2434a630ad83e388a1afe8376edd55ce0a
Adam Gruer <adamgruer@icloud.com>
2019-02-10 20:31:19 +1100

    add data and some cleaning steps

--- data_raw/import.R
+++ data_raw/import.R
@@ -1,18 +1,46 @@
 
 
 library(tidyverse)
-sites <- readxl::read_xlsx("C:/Users/gruera/Downloads/VicRoads_Bike_Site_Number_Listing.xlsx")
+library(fs)
+
+sites <- readxl::read_xlsx("data_raw/VicRoads_Bike_Site_Number_Listing.xlsx")
 View(sites)
 
-sites %>% 
+sites <- 
+  sites %>% 
   janitor::clean_names() %>% 
   select(1:6) %>% 
   filter(!is.na(lat)) %>% 
-  sf::st_as_sf(crs = 4326,coords = c("long","lat")) %>% 
-  mapview::mapview()
-  
-  
+  sf::st_as_sf(crs = 4326,coords = c("long","lat")) 
+
+mapview::mapview(sites)
+
+#unzip each folder to a separate directory
+library(fs)
+
+zip_files <- dir_ls("./data_raw", glob = "*IND.zip") 
+
+walk(zip_files, function(zip_file){
+    new_dir <- 
+          zip_file %>% 
+          path_file() %>% 
+          path_ext_remove() %>% 
+          path("data_raw", .) %>% 
+          unzip(zip_file, exdir = .)
+
+})
+
+dirs <- fs::dir_ls("data_raw", glob = "*IND", type = "directory")
+
+bike_data <- fs::dir_map(dirs, read_csv) %>% 
+   map_dfr(rbind)
+
+ggplot(bike_data, aes(x = SPEED)) +
+geom_histogram(  )
+
+summary(bike_data)
 
 
-bike_volume_speed_url <- "https://vicroadsopendatastorehouse.vicroads.vic.gov.au/opendata/Traffic_Measurement/Bicycle_Volume_and_Speed/Bicycle_Volume_speed_2015.zip"
+
+#bike_volume_speed_url <- "https://vicroadsopendatastorehouse.vicroads.vic.gov.au/opendata/Traffic_Measurement/Bicycle_Volume_and_Speed/Bicycle_Volume_speed_2015.zip"
 
